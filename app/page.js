de26bbd19cbc01ac16b3f0d3025b7e01b9c5a1bf
Sugam Kumar <103991801+gitskr26hub@users.noreply.github.com>
2023-11-27 12:08:10 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -176,7 +176,7 @@ export default function Home() {
 
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-4">
               <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <thead className="text-xs  uppercase bg-gray-700 text-white">
                   <tr>
                     <th scope="col" className="px-6 py-3">
                       Date
