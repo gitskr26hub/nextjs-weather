61705290aa9090e26282147e7733ec6436ca1532
Sugam Kumar <103991801+gitskr26hub@users.noreply.github.com>
2023-11-28 21:58:42 +0530

    Add files via upload

---
+++ components/MonthDay.js
@@ -0,0 +1,49 @@
+
+export const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ]; 
+export const monthNamesHi = [
+    "जनवरी",
+    "फरवरी",
+    "मार्च",
+    "अप्रैल",
+    "मई",
+    "जून",
+    "जुलाई",
+    "अगस्त",
+    "सितम्बर",
+    "अक्टूबर",
+    "नवम्बर",
+    "दिसम्बर",
+  ]; 
+  
+  export const dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  export const dayNamesHi = [
+    "रविवार",
+    "सोमवार",
+    "मंगलवार",
+    "बुधवार",
+    "गुरुवार",
+    "शुक्रवार",
+    "शनिवार",
+  ];
+  
