91a71f3cbb87e672323846bf7f41321b895a0fc9
Sugam Kumar <103991801+gitskr26hub@users.noreply.github.com>
2023-11-28 22:03:34 +0530

    Add files via upload

--- app/api/route.js
+++ app/api/route.js
@@ -3,6 +3,6 @@
 export async function GETBYLOCATION(lati, longi) {
     let res = await fetch(`https://api.open-meteo.com/v1/dwd-icon?latitude=${lati}&longitude=${longi}&current=temperature_2m,is_day&hourly=temperature_2m,relative_humidity_2m,cloud_cover,wind_speed_10m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&timezone=auto`);
       res = await res.json();
-    //  console.log(res);
+      console.log(res);
       return res
 }
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
+
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Context from "@/context/context"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,11 +13,15 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  
   return (
     <html lang="en">
       <body className={inter.className}>
+       <Context>
         <Navbar />
         {children}
+      </Context>
+
       </body>
     </html>
   );
